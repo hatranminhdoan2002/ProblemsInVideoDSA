c5b9c190f7cc2f5b432ea3631f0ece3e6aef7f32
Tony <hatrandoanminh@gmail.com>
2026-03-01 15:21:55 +0700

    Coupling 2 elements of an array

---
+++ BaiTapVideo1.1/BaiTapVideo1.1/BaiTapVideo1.1.cpp
@@ -0,0 +1,36 @@
+// BaiTapVideo1.1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+int main()
+{
+    FILE* fp;
+    freopen_s(&fp,"baitapvideo1.1.inp", "r", stdin);
+    freopen_s(&fp,"baitapvideo1.1.out", "w", stdout);
+    int n,i,j;
+    cin >> n;
+    vector<int> a(n);
+    for (i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            cout << a[i] << a[j] << " ";
+        }
+    }
+    
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
